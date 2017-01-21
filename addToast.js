797ac48b8cb5088311ad1fe5272763adf077b7f2
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2017-01-21 12:28:52 -0600

    toasthere

---
+++ addToast.js
@@ -0,0 +1,18 @@
+var release = require("./src/config.json");
+var min_path="./dist/toast-"+config.version+".min.js";
+var outpath = "Toast.js";
+
+var fs = require('fs');
+require.extensions['.txt'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+var jsfile = require(min_path);
+
+fs.writeFile(outpath, jsfile, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("Toast successfully copied, to use it in your project include the \"Toast.js\" file.");
+});
